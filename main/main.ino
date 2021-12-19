983402471f428f36561c9386ce4ba175cf5cbbdf
Lorenzo Ianotto <lorenzus.ian8@gmail.com>
2021-12-19 21:46:04 +0100

    Abbozzo struttura if-else per il feedback visivo d'inclinazione tramite LED

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,12 @@
 #include <Wire.h>
 #include <Adafruit_NeoPixel.h>
 
-const __uint8_t MPU = 0x68; // I2C address of the MPU-6050
-const __uint8_t PIN_STRISCIA = 4;   // Pin linea dati striscia
-const __uint8_t N_LEDS = 4;
+#define GYRO_MAX_VAL 32750  // Massimo valore positivo letto dal giroscopio
+#define INCLINATION_TOLLERANCE 100  // Tolleranza inclinazione
+
+const uint8_t MPU = 0x68; // I2C address of the MPU-6050
+const uint8_t PIN_STRISCIA = 4;   // Pin linea dati striscia
+const uint8_t N_LEDS = 4; // Numero leds
 __INT16_TYPE__ AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
 
 Adafruit_NeoPixel striscia = Adafruit_NeoPixel(N_LEDS, PIN_STRISCIA, NEO_RGB);
@@ -22,6 +25,9 @@ void setup()
 
 void loop()
 {
+    uint8_t i;
+
+    // TODO: togliere operazioni e variabili inutili
     Wire.beginTransmission(MPU);
     Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
     Wire.endTransmission();
@@ -47,6 +53,55 @@ void loop()
     Serial.print(" | Y = "); Serial.print(GyY);
     Serial.print(" | Z = "); Serial.println(GyZ);
     Serial.println(" ");
+    
+    // TODO: num pixels solamente indicativi
+    // La struttura è una bozza, da ottimizzare
+    if(GyX > INCLINATION_TOLLERANCE &&  GyY > INCLINATION_TOLLERANCE)   // Angolo 0 in alto
+    {
+        striscia.setPixelColor(0, striscia.Color(0, (255*GYRO_MAX_VAL*2)/(GyX + GYRO_MAX_VAL), (255*GYRO_MAX_VAL*2)/(GyY + GYRO_MAX_VAL))); // g,r,b CONTROLLA SE è VERO
+    }
+    else if(GyX > INCLINATION_TOLLERANCE && GyY < INCLINATION_TOLLERANCE*(-1))  // Angolo 1 in alto
+    {
+        striscia.setPixelColor(1, striscia.Color(0, (255*GYRO_MAX_VAL*2)/(GyX + GYRO_MAX_VAL), (255*GYRO_MAX_VAL*2)/(GyY + GYRO_MAX_VAL)));
+    }
+    else if(GyX < INCLINATION_TOLLERANCE*(-1) && GyY > INCLINATION_TOLLERANCE)  // Angolo 2 in alto
+    {
+        striscia.setPixelColor(2, striscia.Color(0, (255*GYRO_MAX_VAL*2)/(GyX + GYRO_MAX_VAL), (255*GYRO_MAX_VAL*2)/(GyY + GYRO_MAX_VAL)));
+    }
+    else if(GyX < INCLINATION_TOLLERANCE*(-1) && GyY < INCLINATION_TOLLERANCE*(-1)) // Angolo 3 in alto
+    {
+        striscia.setPixelColor(3, striscia.Color(0, (255*GYRO_MAX_VAL*2)/(GyX + GYRO_MAX_VAL), (255*GYRO_MAX_VAL*2)/(GyY + GYRO_MAX_VAL)));
+    }
+    else if(GyX > INCLINATION_TOLLERANCE)   // Lato 01 in alto
+    {
+        striscia.setPixelColor(0, striscia.Color(0, (255*GYRO_MAX_VAL*2)/(GyX + GYRO_MAX_VAL), (255*GYRO_MAX_VAL*2)/(GyY + GYRO_MAX_VAL)));
+        striscia.setPixelColor(1, striscia.Color(0, (255*GYRO_MAX_VAL*2)/(GyX + GYRO_MAX_VAL), (255*GYRO_MAX_VAL*2)/(GyY + GYRO_MAX_VAL)));
+    }
+    else if(GyX < INCLINATION_TOLLERANCE*(-1))  // Lato 23 in alto
+    {
+        striscia.setPixelColor(2, striscia.Color(0, (255*GYRO_MAX_VAL*2)/(GyX + GYRO_MAX_VAL), (255*GYRO_MAX_VAL*2)/(GyY + GYRO_MAX_VAL)));
+        striscia.setPixelColor(3, striscia.Color(0, (255*GYRO_MAX_VAL*2)/(GyX + GYRO_MAX_VAL), (255*GYRO_MAX_VAL*2)/(GyY + GYRO_MAX_VAL)));
+    }
+    else if(GyY > INCLINATION_TOLLERANCE)   // Lato 02 in alto
+    {
+        striscia.setPixelColor(0, striscia.Color(0, (255*GYRO_MAX_VAL*2)/(GyX + GYRO_MAX_VAL), (255*GYRO_MAX_VAL*2)/(GyY + GYRO_MAX_VAL)));
+        striscia.setPixelColor(2, striscia.Color(0, (255*GYRO_MAX_VAL*2)/(GyX + GYRO_MAX_VAL), (255*GYRO_MAX_VAL*2)/(GyY + GYRO_MAX_VAL)));
+    }
+    else if(GyY < INCLINATION_TOLLERANCE*(-1))  // Lato 13 in alto
+    {
+        striscia.setPixelColor(1, striscia.Color(0, (255*GYRO_MAX_VAL*2)/(GyX + GYRO_MAX_VAL), (255*GYRO_MAX_VAL*2)/(GyY + GYRO_MAX_VAL)));
+        striscia.setPixelColor(3, striscia.Color(0, (255*GYRO_MAX_VAL*2)/(GyX + GYRO_MAX_VAL), (255*GYRO_MAX_VAL*2)/(GyY + GYRO_MAX_VAL)));
+    }
+    else    // Tutto in bolla
+    {
+        for(i=0; i < N_LEDS; i++)
+        {
+            striscia.setPixelColor(i, striscia.Color(0, 0, 0));
+        }
+    }
+
+
+    striscia.show();
     delay(350);
 }
 
